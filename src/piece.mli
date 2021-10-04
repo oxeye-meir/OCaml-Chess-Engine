c1a2a44002de72bd70203b556ada47ded1e9dac9
Robert Zhao <rz87@cornell.edu>
2021-10-04 15:59:21 -0400

    Added next_moves in piece

--- src/piece.mli
+++ src/piece.mli
@@ -8,3 +8,7 @@ type color = bool
 val position : t -> int * int
 (** [position p] is a tuple representing the position of piece p on the
     board. *)
+
+val next_moves : t -> (int * int) list
+(** [next_moves p] is a tuple list representing the possible next moves
+    of piece p on the board. *)
