32d12a9b53867293039f09fb890d88dd41d14c0d
xh245@cornell.edu <xh245@cornell.edu>
2021-10-04 15:09:19 -0400

    Add some more files

--- test/main.ml
+++ test/main.ml
@@ -1,3 +1,4 @@
 open OUnit2
 
-let sample_test name expected_output input = name >:: fun _ -> assert_equal expected_output input
+let sample_test name expected_output input =
+  name >:: fun _ -> assert_equal expected_output input
