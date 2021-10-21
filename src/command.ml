73d24d5ca3702c128880c34e679f9c80bd46bed7
Robert Zhao <rz87@cornell.edu>
2021-10-21 17:21:56 -0400

    Fix Semantics of Command Coordinates

--- src/command.ml
+++ src/command.ml
@@ -2,46 +2,49 @@ exception Empty
 
 exception Malformed
 
-type color = Black | White
+type color =
+  | Black
+  | White
 
 type position = int * int
 
 type command =
   | Color of color
   | Move of position * position
-  | Quit 
+  | Quit
 
-let format str = 
-  str |> String.trim |> String.lowercase_ascii
+let format str = str |> String.trim |> String.lowercase_ascii
 
 let char_in_range = function
   | 'a' .. 'h' -> true
-  |  _ -> false
+  | _ -> false
 
 let int_in_range = function
   | '0' .. '7' -> true
-  |  _ -> false
+  | _ -> false
 
-let pos_of_str str = 
+let pos_of_str str =
   let ch1 = String.get str 0 in
   let ch2 = String.get str 1 in
-  if char_in_range ch1 && int_in_range ch2 && String.length str = 2 then (Char.code ch1 - 97, Char.code ch2 - 48)
+  if char_in_range ch1 && int_in_range ch2 && String.length str = 2 then
+    (7 - (Char.code ch2 - 49), Char.code ch1 - 97)
   else raise Malformed
 
 let rec remove_empty = function
   | [] -> []
-  | h::t -> if h = "" then remove_empty t else h::remove_empty t
+  | h :: t -> if h = "" then remove_empty t else h :: remove_empty t
 
-let parse str = 
+let parse str =
   let s = format str in
-    let words = remove_empty (String.split_on_char ' ' s) in
-      let num_of_words = List.length words in
-       if num_of_words = 0 then raise Empty
-       else if num_of_words = 1 then let word = List.nth words 0 in
-          if word = "quit" then Quit
-          else if word = "white" then Color White
-          else if word = "black" then Color Black
-          else raise Malformed
-       else if num_of_words = 2 then Move ((pos_of_str (List.nth words 0) ), (pos_of_str (List.nth words 1 )))
-       else raise Malformed
-
+  let words = remove_empty (String.split_on_char ' ' s) in
+  let num_of_words = List.length words in
+  if num_of_words = 0 then raise Empty
+  else if num_of_words = 1 then
+    let word = List.nth words 0 in
+    if word = "quit" then Quit
+    else if word = "white" then Color White
+    else if word = "black" then Color Black
+    else raise Malformed
+  else if num_of_words = 2 then
+    Move (pos_of_str (List.nth words 0), pos_of_str (List.nth words 1))
+  else raise Malformed
--- src/command.mli
+++ src/command.mli
@@ -4,47 +4,45 @@ exception Empty
 exception Malformed
 (** Raised when the inputed command is malformed. *)
 
-type color = Black | White
-(** The type [color] represents the color of the piece that a player chooses
-    at the start of the game. *)
+(** The type [color] represents the color of the piece that a player chooses at the start of
+    the game. *)
+type color =
+  | Black
+  | White
 
 type position = int * int
-(** The type [position] represents a position on the board (x,y) where x and y
-    are both integer in the range 0-7 inclusive. *)
+(** The type [position] represents a position on the board (x,y) where x and y are both integer
+    in the range 0-7 inclusive. *)
 
+(** The type [command] is a player's command which is then parsed into either a color of the
+    piece the player chose, the quit command or the position to which a player wantes to move a
+    piece. There will be more commands later on - these are the only commands for now. *)
 type command =
   | Color of color
   | Move of position * position
   | Quit
-(** The type [command] is a player's command which is then parsed into either a 
-    color of the piece the player chose, the quit command or the position to which a 
-    player wantes to move a piece. There will be more commands later on - these are 
-    the only commands for now. *)
 
 val parse : string -> command
-(** [parse str] parses a player's input [str] into a [command], as follows.
-    If [str] is one word, it is assumed that the user is inputting either the 
-    color of the piece or the quit command. Their input is checked to be a valid
-    color (white or black) or quit, if their input is valid, the corresponding 
-    command is returned.
-    If [str] is two words, it is assumed to be a move from one position to another. 
-    The positions are checked to be valid and if they are, the move command is returned.
+(** [parse str] parses a player's input [str] into a [command], as follows. If [str] is one
+    word, it is assumed that the user is inputting either the color of the piece or the quit
+    command. Their input is checked to be a valid color (white or black) or quit, if their
+    input is valid, the corresponding command is returned. If [str] is two words, it is assumed
+    to be a move from one position to another. The positions are checked to be valid and if
+    they are, the move command is returned.
 
     Examples:
-    - [parse "a3 c6"] is [Move (0,3),(2,6)]
+
+    - [parse "a3 c6"] is [Move (5,0),(2,2)]
     - [parse "Black"] is [Color Black].
     - [parse "Quit"] is [Quit].
 
-    Requires: [str] contains only alphanumeric (A-Z, a-z, 0-9) and space
-    characters (only ASCII character code 32; not tabs or newlines,
-    etc.).
+    Requires: [str] contains only alphanumeric (A-Z, a-z, 0-9) and space characters (only ASCII
+    character code 32; not tabs or newlines, etc.).
 
-    Raises: [Empty] if [str] is the empty string or contains only
-    spaces.
+    Raises: [Empty] if [str] is the empty string or contains only spaces.
 
-    Raises: [Malformed] if [str] is malformed. An input string is
-    malformed if it is not either two valid positions where each position
-    consists of a character from a-h inclusive and an int from 0-7 inclusive
-    and positions are seperated by at least one space, nor string "white", 
-    "black", or "quit" (where letters can be capital, lowercase, and spaces 
-    can lead and trail the string). *)
+    Raises: [Malformed] if [str] is malformed. An input string is malformed if it is not either
+    two valid positions where each position consists of a character from a-h inclusive and an
+    int from 0-7 inclusive and positions are seperated by at least one space, nor string
+    "white", "black", or "quit" (where letters can be capital, lowercase, and spaces can lead
+    and trail the string). *)
