7640480e08ae16f539c109ba13c3a64b311ef68b
Robert Zhao <rz87@cornell.edu>
2021-10-21 17:20:37 -0400

    Finished main loop

--- bin/main.ml
+++ bin/main.ml
@@ -4,22 +4,19 @@ open Board
 
 type position = int * int
 
-(** Chess.Piece, Chess.Board: pieces =
-    [Pawn B/W, Rook B/W, Knight B/W, Bishop B/W, Queen B/W, King B/W, Empty] *)
-
-let print_quit () = ANSITerminal.print_string [ ANSITerminal.red ] "QUITTING .... \n"
+let print_quit () = ANSITerminal.print_string [ ANSITerminal.yellow ] "QUITTING .... \n"
 
 let print_rules () =
   ANSITerminal.print_string [ ANSITerminal.green ]
     "\n\
      Rules\n\
      1. To move your piece, please write \n\
-    \     “current_position new_position”, i.e. “e2 e4”\n\n\
+    \     “current_position new_position”, i.e. e2 e4”\n\n\
      2. To quit, type 'Quit.'\n\n"
 
 (*print_invalid_move is not used anywhere yet*)
 let print_invalid_move () =
-  ANSITerminal.print_string [ ANSITerminal.magenta ] "Invalid move. Please try again \n";
+  ANSITerminal.print_string [ ANSITerminal.red ] "Invalid move. Please try again \n";
   print_string "> "
 
 let init_board_print () =
@@ -40,20 +37,33 @@ let get_command (input : string) : position * position =
       print_quit ();
       exit 0
 
-let rec get_board board =
-  let input = read_line () in
-  let command = get_command input in
-  let new_board = Board.move board (fst command) (snd command) in
-  print_board new_board
+(* let position_printer (x, y) = "(" ^ string_of_int x ^ ", " ^ string_of_int y ^ ")" *)
 
-let play_game f = print_endline "Game started"
+let rec get_current_board board =
+  print_board board;
+  print_string "Your move> ";
+  let input = read_line () |> String.trim in
+  let command = get_command input in
+  let start_coord = fst command in
+  (* position_printer start_coord |> print_string; *)
+  let end_coord = snd command in
+  (* position_printer start_coord |> print_string; *)
+  let next_board =
+    try Chess.Board.move board start_coord end_coord with
+    | InvalidPos ->
+        print_invalid_move ();
+        board
+  in
+  get_current_board next_board
+
+let initial_state = Chess.Board.init_board
 
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
   ANSITerminal.print_string [ ANSITerminal.blue ] "\n\nWelcome to the Chess Game engine.\n";
-  init_board_print ();
   print_rules ();
-  print_string "> "
+
+  get_current_board initial_state
 
 (* Execute the game engine. *)
 let () = main ()
